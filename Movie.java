6fe2e8791ff8c3c1b633f3ba36fcbc9c8995459e
Raphael Augusto <raphaelaugustoteixeirasilva@gmail.com>
2020-10-19 01:07:42 +0000

    Commit 14

--- Movie.java
+++ Movie.java
@@ -36,17 +36,11 @@ public class Movie {
       return _title;
   }
 
-
    public double getCharge(int daysRented) {
          return _price.getCharge(daysRented);
    }
-  public int getFrequentRenterPoints(int daysRented) {
-    // add bonus for a two day new release rental
-    if (getPriceCode()  == Movie.NEW_RELEASE && daysRented > 1) {
-      return 2;
-    } else {
-      return 1;
-    }
-  }
 
+  public int getFrequentRenterPoints(int daysRented) {
+      return _price.getFrequentRenterPoints(daysRented);
+  }
 }
